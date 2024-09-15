a5c31c391614a5d14ca100006835de6b21043482
ArthurPeruzzo <arthur.kohl@viasoft.com.br>
2024-09-15 13:10:49 -0300

    animal-search

--- src/app/animal-search/animal-search.module.ts
+++ src/app/animal-search/animal-search.module.ts
@@ -11,6 +11,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -27,7 +28,9 @@ import { MatSelectModule } from '@angular/material/select';
     MatCardModule,
     MatOptionModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatIconModule
+
   ], exports: [AnimalSearchComponent]
 })
 export class AnimalSearchModule { }
